c6d08058bc3899689ea993a01d01466a96ade4ae
TrueGuardian32 <MincraftNoob2012@gmail.com>
2018-04-27 14:56:05 -0500

    Update VoteSystemAlpha.java

--- VoteSystemAlpha.java
+++ VoteSystemAlpha.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 public class VoteSystemAlpha {
 	public static void main(String[] args) {
-		int vca = 0, vcb = 0, vcc = 0;
+		int votecountalpha = 0, votecountbeta = 0, votecountcharlie = 0, votecountmax = 0;
+		String VoteFor1 = "", VoteFor2 = "", VoteFor3 = "", VoteFor4 = "", VoteFor5 = "";
+		
+		//Variables above 
 		try
 		{
 			Scanner inFile = new Scanner(new FileReader("\\src\\votesystem.txt"));
 		}
-		catch (Exception ex)
+		catch (Exception ex) //Catches Errors
 		{
 			System.out.println(ex.toString());
 		}
 	}
 }
+
